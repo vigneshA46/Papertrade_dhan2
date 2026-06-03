545d699d6f6f102654e07c7af3d8c6b2753b1e3f
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-06-03 10:33:36 +0530

    sfddsf'

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ import os
 import pytz
 import threading
 import time
+
+try:
+    import paper_trade_niftyoption50_no_reentry as strategy1
+except Exception as e:
+    print("strategy1 ERROR:", e)
+
+    
 #import paper_trade_niftyoption50_reentry as strategy2
 #import paper_trade_niftyoption35_reentry as strategy3
 #import paper_trade_niftyoption35_reentry_point as strategy4
 #import paper_trade_niftyoption50_reentry_point as strategy5
 #import nifty_option_buying_50_ltp as strategy15
-import nifty_option_buying_cumulative_ltp as straegy16
+##import nifty_option_buying_cumulative_ltp as straegy16
 
 import delta_option_buying as strategy6
 #import paper_trade_niftyoption8_no_reentry as strategy8
@@ -25,11 +32,6 @@ try:
 except Exception as e:
     print("strategy7 ERROR:", e)
 
-
-try:
-    import paper_trade_niftyoption50_no_reentry as strategy1
-except Exception as e:
-    print("strategy1 ERROR:", e)
 
 
 rb_started = False
